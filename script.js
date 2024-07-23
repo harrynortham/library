978617ee7b4f804cc1e4d786e808a9708caed31e
Harry Northam <hello@harrynortham.com>
2024-07-23 09:41:03 +0200

    Add prototype and eventlistener to change book object to read when clicked

--- script.js
+++ script.js
@@ -1,28 +1,59 @@
+const booksContainer = document.getElementById("books-container");
 const addBookButton = document.getElementById("add-book");
 const addBookDialog = document.getElementById("add-book__dialog");
 const addBookForm = document.getElementById("add-book__form");
 const formError = document.getElementsByClassName("formError")[0];
-const bookName = document.getElementById("add-book__name");
-const bookAuthor = document.getElementById("add-book__author");
-const bookPages = document.getElementById("add-book__pages");
+const nameInput = document.getElementById("add-book__name");
+const authorInput = document.getElementById("add-book__author");
+const pagesInput = document.getElementById("add-book__pages");
 
 const myLibrary = [];
+
+const bookPrototype = {
+  read: function () {
+    this.isRead = true;
+    console.log(myLibrary);
+  },
+};
 
 function Book(name, author, pages) {
   // the constructor...
   this.name = name;
   this.author = author;
   this.pages = pages;
+  this.isRead = false;
 }
 
-function addBookToLibrary() {
+Object.assign(Book.prototype, bookPrototype);
+
+function showBooks() {
+  // if already exists, delete all child nodes
+  booksContainer.innerHTML = "";
+  myLibrary.map((book) => {
+    //create each element add to the contaienr
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = `<div>${book.name}</div> <button>Read</button>`;
+
+    newDiv.addEventListener("click", () => {
+      book.read();
+    });
+    booksContainer.appendChild(newDiv);
+  });
+}
+
+function addBookToLibrary(name, author, pages) {
   // do stuff here
+  let book = new Book(name, author, pages);
+  myLibrary.push(book);
+  console.log(myLibrary);
+  //refresh the dom elements
+  showBooks();
 }
 
 function closeDialog() {
-  bookName.value = "";
-  bookAuthor.value = "";
-  bookPages.value = "";
+  nameInput.value = "";
+  authorInput.value = "";
+  pagesInput.value = "";
 
   addBookDialog.close();
 }
@@ -34,17 +65,14 @@ addBookButton.addEventListener("click", () => {
 addBookForm.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  if (bookName.value == "" || bookAuthor.value == "" || bookPages.value == "") {
+  if (
+    nameInput.value == "" ||
+    authorInput.value == "" ||
+    pagesInput.value == ""
+  ) {
     formError.textContent = "Fields can't be empty.";
   } else {
-    let book = new Book(bookName.value, bookAuthor.value, bookPages.value);
-
-    myLibrary.push(book);
-
+    addBookToLibrary(nameInput.value, authorInput.value, pagesInput.value);
     closeDialog();
-
-    //refresh the dom elements
-
-    console.log(myLibrary);
   }
 });
